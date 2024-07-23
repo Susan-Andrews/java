b3a116ad988c81dd188c2f389fb628daed63305f
Susan Andrews <susanandrews2001@gmail.com>
2024-07-23 19:03:46 +0530

    Add files via upload

---
+++ java/ExceptionHandling.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List; // Import the List interface
+public class ExceptionHandling{
+    public void writeList(){
+
+        try{
+            List<Integer> list = new ArrayList<>(10);
+            list.add(10);
+            System.out.println("In the try statement");
+            Integer a =list.get(1);
+            System.out.println("first element: " + a); 
+        }
+        catch(IndexOutOfBoundsException e){
+            System.err.println("Index out of bound exception " + e.getMessage() );
+        }
+        finally{
+            System.out.println("execute always"); 
+
+        }
+    }
+
+    public static void main(String args[]){
+        ExceptionHandling ex=new ExceptionHandling();
+        ex.writeList();
+    }
+}
